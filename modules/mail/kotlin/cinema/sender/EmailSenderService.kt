c1adb48a9855d1c038e77e9bb6398987a7a12bff
kamil <wujeckamil@gmail.com>
2018-04-17 21:57:25 +0200

    mailer, switch from component to configuration

--- modules/mail/kotlin/cinema/sender/EmailSenderService.kt
+++ modules/mail/kotlin/cinema/sender/EmailSenderService.kt
@@ -2,12 +2,12 @@ package cinema.sender
 
 import cinema.email.Email
 import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 
 @Service
-class EmailSenderService(private val cinemaMailSender: CinemaMailSender) {
-    fun send(email: Email) = cinemaMailSender
-            .getYahooMailSender()
+class EmailSenderService(private val mailSender: JavaMailSender) {
+    fun send(email: Email) = mailSender
             .send(buildMimeMessage(email))
 
     private fun buildMimeMessage(email: Email) = SimpleMailMessage()
